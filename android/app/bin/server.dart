75917c9f72c4de9f9763894bb7eefd901fee86dc
Vrushabh Tawde <vrushabhtawde0552@gmail.com>
2023-03-26 14:58:21 +0530

    go

---
+++ android/app/bin/server.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:appengine/appengine.dart';
+
+requestHandler(HttpRequest request) {
+  request.response
+      ..write('Hello, world!')
+      ..close();
+}
+
+main() async {
+  
+  await runAppEngine(requestHandler);
+}
